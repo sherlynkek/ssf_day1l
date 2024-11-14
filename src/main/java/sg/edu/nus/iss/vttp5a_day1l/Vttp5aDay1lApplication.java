0131d92648116645209f890405437a249298c5a5
sherlynkek <sherlynkek00@gmail.com>
2024-11-14 09:46:46 +0800

    README.md

---
+++ src/main/java/sg/edu/nus/iss/vttp5a_day1l/Vttp5aDay1lApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vttp5a_day1l;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vttp5aDay1lApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vttp5aDay1lApplication.class, args);
+	}
+
+}
